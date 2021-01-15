46822ed84f9e81776a468a6b8f98aba6a17dce67
Marian Wulf <77483470+marianwulf@users.noreply.github.com>
2021-01-15 18:41:31 +0100

    added Fish V1.0.1 Script

---
+++ Fish/Fish_StreamlabsSystem.py
@@ -0,0 +1,323 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name
+"""Rob command, replica of the textbased version."""
+#---------------------------------------
+# Libraries and references
+#---------------------------------------
+import codecs
+import json
+import os
+import winsound
+import ctypes
+from array import *
+#---------------------------------------
+# [Required] Script information
+#---------------------------------------
+ScriptName = "Fish"
+Website = "https://www.twitch.tv/maroxtv"
+Creator = "Marox"
+Version = "1.0.1"
+Description = "Fish command"
+#---------------------------------------
+# Versions
+#---------------------------------------
+""" Releases (open README.txt for full release notes)
+1.0.0 - Initial Release
+1.0.1 - fixed cost
+"""
+#---------------------------------------
+# Variables
+#---------------------------------------
+settingsFile = os.path.join(os.path.dirname(__file__), "settings.json")
+
+#---------------------------------------
+# Classes
+#---------------------------------------
+class Settings:
+    """" Loads settings from file if file is found if not uses default values"""
+
+    # The 'default' variable names need to match UI_Config
+    def __init__(self, settingsFile=None):
+        if settingsFile and os.path.isfile(settingsFile):
+            with codecs.open(settingsFile, encoding='utf-8-sig', mode='r') as f:
+                self.__dict__ = json.load(f, encoding='utf-8-sig')
+
+        else: #set variables if no custom settings file is found
+            self.OnlyLive = False
+            self.Command = "!fish"
+            self.Cost = 0
+            self.Permission = "Everyone"
+            self.PermissionInfo = ""
+            self.Usage = "Stream Chat"
+            self.UseCD = True
+            self.Cooldown = 5
+            self.OnCooldown = "{0} the command is still on cooldown for {1} seconds!"
+            self.UserCooldown = 10
+            self.OnUserCooldown = "{0} the command is still on user cooldown for {1} seconds!"
+            self.CasterCD = True
+            self.NotEnoughResponse = "{0} you don't have enough {1} to attempt this! You will need atleast {2} {1}."
+            self.PermissionResponse = "{0} -> only {1} ({2}) and higher can use this command"
+            self.Timeout = False
+            self.TL = 60
+            self.B1Name = "Loserforelle"
+            self.B1WinChance = 80
+            self.B1Win = 7
+            self.B1Lose = 3
+            self.B1StartText = "{0} du trittst gegen [Weiß] Loserforelle an. Viel Glück!"
+            self.B1WinText = "{0} du hast {1} {2} gewonnen! .... und stinkst LuL."
+            self.B1LoseText = "{0} du hast {1} {2} verloren! Loserforelle macht ihrem Namen alle Ehre. (Noobfilter)"
+            self.B2Name = "Öderkarpfen"
+            self.B2WinChance = 60
+            self.B2Win = 15
+            self.B2Lose = 15
+            self.B2StartText = "{0} du trittst gegen [Grün] Öderkarpfen an. Viel Glück!"
+            self.B2WinText = "{0} du hast {1} {2} gewonnen!"
+            self.B2LoseText = "{0} du hast {1} {2} verloren!"
+            self.B3Name = "Parketthering"
+            self.B3WinChance = 40
+            self.B3Win = 30
+            self.B3Lose = 18
+            self.B3StartText = "{0} du trittst gegen [Blau] Parketthering an. Viel Glück!"
+            self.B3WinText = "{0} du hast {1} {2} gewonnen!"
+            self.B3LoseText = "{0} du hast {1} {2} verloren!"
+            self.B4Name = "Geilerhecht"
+            self.B4WinChance = 20
+            self.B4Win = 40
+            self.B4Lose = 13
+            self.B4StartText = "{0} du trittst gegen [Lila] Geilerhecht an. Viel Glück!"
+            self.B4WinText = "{0} du hast {1} {2} gewonnen!"
+            self.B4LoseText = "{0} du hast {1} {2} verloren!"
+            self.B5Name = "Saltyrohrtopus"
+            self.B5WinChance = 10
+            self.B5Win = 80
+            self.B5Lose = 15
+            self.B5StartText = "{0} du trittst gegen [Gold] Saltyrohrtopus an. Viel Glück!"
+            self.B5WinText = "{0} du hast {1} {2} gewonnen!"
+            self.B5LoseText = "{0} du hast {1} {2} verloren!"
+
+    # Reload settings on save through UI
+    def ReloadSettings(self, data):
+        """Reload settings on save through UI"""
+        self.__dict__ = json.loads(data, encoding='utf-8-sig')
+        return
+
+    # Save settings to files (json and js)
+    def SaveSettings(self, settingsFile):
+        """Save settings to files (json and js)"""
+        with codecs.open(settingsFile, encoding='utf-8-sig', mode='w+') as f:
+            json.dump(self.__dict__, f, encoding='utf-8-sig')
+        with codecs.open(settingsFile.replace("json", "js"), encoding='utf-8-sig', mode='w+') as f:
+            f.write("var settings = {0};".format(json.dumps(self.__dict__, encoding='utf-8-sig', ensure_ascii=False)))
+        return
+#---------------------------------------
+# [OPTIONAL] Settings functions
+#---------------------------------------
+def SetDefaults():
+    """Set default settings function"""
+
+    #play windows sound
+    winsound.MessageBeep()
+
+    #open messagebox with a security check
+    MessageBox = ctypes.windll.user32.MessageBoxW
+    returnValue = MessageBox(0, u"You are about to reset the settings, "
+                                "are you sure you want to contine?"
+                             , u"Reset settings file?", 4)
+
+    #if user press "yes"
+    if returnValue == 6:
+
+        # Save defaults back to file
+        Settings.SaveSettings(MySet, settingsFile)
+
+        #show messagebox that it was complete
+        MessageBox = ctypes.windll.user32.MessageBoxW
+        returnValue = MessageBox(0, u"Settings successfully restored to default values"
+                                 , u"Reset complete!", 0)
+
+#---------------------------------------
+# [Required] functions
+#---------------------------------------
+def Init():
+    """data on Load, required function"""
+    global MySet
+    MySet = Settings(settingsFile)
+
+def Execute(data):
+    """Required Execute data function"""
+    if data.IsChatMessage() and data.GetParam(0).lower() == MySet.Command.lower():
+
+        if not IsFromValidSource(data, MySet.Usage):
+            return
+
+        if not Parent.HasPermission(data.User, MySet.Permission, MySet.PermissionInfo):
+            message = MySet.PermissionResponse.format(data.User, MySet.Permission, MySet.PermissionInfo)
+            SendResp(data, message)
+
+        if not HasPermission(data):
+            return
+
+        if not MySet.OnlyLive or Parent.IsLive():
+
+            if IsOnCooldown(data):
+                return
+
+            
+            
+            Boss = [[MySet.B1Name, MySet.B1WinChance, MySet.B1Win, MySet.B1Lose, MySet.B1StartText.format(data.UserName), MySet.B1WinText.format(data.UserName, MySet.B1Win, Parent.GetCurrencyName()), MySet.B1LoseText.format(data.UserName, MySet.B1Lose, Parent.GetCurrencyName())], \
+                    [MySet.B2Name, MySet.B2WinChance, MySet.B2Win, MySet.B2Lose, MySet.B2StartText.format(data.UserName), MySet.B2WinText.format(data.UserName, MySet.B2Win, Parent.GetCurrencyName()), MySet.B2LoseText.format(data.UserName, MySet.B2Lose, Parent.GetCurrencyName())], \
+                    [MySet.B3Name, MySet.B3WinChance, MySet.B3Win, MySet.B3Lose, MySet.B3StartText.format(data.UserName), MySet.B3WinText.format(data.UserName, MySet.B3Win, Parent.GetCurrencyName()), MySet.B3LoseText.format(data.UserName, MySet.B3Lose, Parent.GetCurrencyName())], \
+                    [MySet.B4Name, MySet.B4WinChance, MySet.B4Win, MySet.B4Lose, MySet.B4StartText.format(data.UserName), MySet.B4WinText.format(data.UserName, MySet.B4Win, Parent.GetCurrencyName()), MySet.B4LoseText.format(data.UserName, MySet.B4Lose, Parent.GetCurrencyName())], \
+                    [MySet.B5Name, MySet.B5WinChance, MySet.B5Win, MySet.B5Lose, MySet.B5StartText.format(data.UserName), MySet.B5WinText.format(data.UserName, MySet.B5Win, Parent.GetCurrencyName()), MySet.B5LoseText.format(data.UserName, MySet.B5Lose, Parent.GetCurrencyName())]]            
+            
+            highestlose = MySet.B1Lose
+            
+            for BossIT in Boss:
+                if BossIT[3] > highestlose:
+                    highestlose = BossIT[3]
+            
+            if not Parent.RemovePoints(data.User, data.UserName, highestlose + MySet.Cost):
+                message = MySet.NotEnoughResponse.format(data.UserName, Parent.GetCurrencyName(), highestlose + MySet.Cost)
+                SendResp(data, message)
+                return
+            Parent.AddPoints(data.User, data.UserName, highestlose + MySet.Cost)
+            
+            Parent.RemovePoints(data.User, data.UserName, MySet.Cost)
+            
+            selectedboss = Parent.GetRandom(0,5)
+            selectedwin = Parent.GetRandom(1,101)
+            
+            Boss = Boss[selectedboss]
+            
+            message = Boss[4]
+            SendResp(data, message)
+            
+            if selectedwin <= Boss[1]:
+                Parent.AddPoints(data.User, data.UserName, Boss[2])
+                message = Boss[5]
+                SendResp(data, message)
+                AddCooldown(data)
+                return
+            elif selectedwin > Boss[1]:
+                Parent.RemovePoints(data.User, data.UserName, Boss[3])
+                message = Boss[6]
+                SendResp(data, message)
+                AddCooldown(data)
+                if MySet.Timeout:
+                    Parent.SendStreamMessage("/timeout {0} {1}".format(data.User, MySet.TL))
+                return
+            else:
+                message = "Fish hat nen Bug :("
+                SendResp(data, message)
+
+def Tick():
+    """Required tick function"""
+
+#---------------------------------------
+# [Optional] Functions for usage handling
+#---------------------------------------
+def SendResp(data, sendMessage):
+    """Sends message to Stream or discord chat depending on settings"""
+
+    if not data.IsFromDiscord() and not data.IsWhisper():
+        Parent.SendStreamMessage(sendMessage)
+
+    if not data.IsFromDiscord() and data.IsWhisper():
+        Parent.SendStreamWhisper(data.User, sendMessage)
+
+    if data.IsFromDiscord() and not data.IsWhisper():
+        Parent.SendDiscordMessage(sendMessage)
+
+    if data.IsFromDiscord() and data.IsWhisper():
+        Parent.SendDiscordDM(data.User, sendMessage)
+
+def CheckUsage(data, rUsage):
+    """Return true or false depending on the message is sent from
+    a source that's in the usage setting or not"""
+
+    if not data.IsFromDiscord():
+        l = ["Stream Chat", "Chat Both", "All", "Stream Both"]
+        if not data.IsWhisper() and (rUsage in l):
+            return True
+
+        l = ["Stream Whisper", "Whisper Both", "All", "Stream Both"]
+        if data.IsWhisper() and (rUsage in l):
+            return True
+
+    if data.IsFromDiscord():
+        l = ["Discord Chat", "Chat Both", "All", "Discord Both"]
+        if not data.IsWhisper() and (rUsage in l):
+            return True
+
+        l = ["Discord Whisper", "Whisper Both", "All", "Discord Both"]
+        if data.IsWhisper() and (rUsage in l):
+            return True
+
+    return False
+
+def IsOnCooldown(data):
+    """Return true if command is on cooldown and send cooldown message if enabled"""
+    cooldown = Parent.IsOnCooldown(ScriptName, MySet.Command)
+    userCooldown = Parent.IsOnUserCooldown(ScriptName, MySet.Command, data.User)
+    caster = (Parent.HasPermission(data.User, "Caster", "") and MySet.CasterCD)
+
+    if (cooldown or userCooldown) and caster is False:
+
+        if MySet.UseCD:
+            cooldownDuration = Parent.GetCooldownDuration(ScriptName, MySet.Command)
+            userCDD = Parent.GetUserCooldownDuration(ScriptName, MySet.Command, data.User)
+
+            if cooldownDuration > userCDD:
+                m_CooldownRemaining = cooldownDuration
+
+                message = MySet.OnCooldown.format(data.UserName, m_CooldownRemaining)
+                SendResp(data, message)
+
+            else:
+                m_CooldownRemaining = userCDD
+
+                message = MySet.OnUserCooldown.format(data.UserName, m_CooldownRemaining)
+                SendResp(data, message)
+        return True
+    return False
+
+def HasPermission(data):
+    """Returns true if user has permission and false if user doesn't"""
+    if not Parent.HasPermission(data.User, MySet.Permission, MySet.PermissionInfo):
+        message = MySet.PermissionResponse.format(data.UserName, MySet.Permission, MySet.PermissionInfo)
+        SendResp(data, message)
+        return False
+    return True
+
+def IsFromValidSource(data, Usage):
+    """Return true or false depending on the message is sent from
+    a source that's in the usage setting or not"""
+    if not data.IsFromDiscord():
+        l = ["Stream Chat", "Chat Both", "All", "Stream Both"]
+        if not data.IsWhisper() and (Usage in l):
+            return True
+
+        l = ["Stream Whisper", "Whisper Both", "All", "Stream Both"]
+        if data.IsWhisper() and (Usage in l):
+            return True
+
+    if data.IsFromDiscord():
+        l = ["Discord Chat", "Chat Both", "All", "Discord Both"]
+        if not data.IsWhisper() and (Usage in l):
+            return True
+
+        l = ["Discord Whisper", "Whisper Both", "All", "Discord Both"]
+        if data.IsWhisper() and (Usage in l):
+            return True
+    return False
+
+def AddCooldown(data):
+    """add cooldowns"""
+    if Parent.HasPermission(data.User, "Caster", "") and MySet.CasterCD:
+        Parent.AddCooldown(ScriptName, MySet.Command, MySet.Cooldown)
+        return
+
+    else:
+        Parent.AddUserCooldown(ScriptName, MySet.Command, data.User, MySet.UserCooldown)
+        Parent.AddCooldown(ScriptName, MySet.Command, MySet.Cooldown)
